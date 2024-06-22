07603348ae3578e27ca599c8148d6ab82cfd4fda
as.medvedeff <as.medvedeff96@gmail.com>
2024-06-22 17:42:46 +0500

    models added

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -6,17 +6,47 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    ...
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.username}-{self.text[:50]}'
 
 
-# для доп. задания
-# class PostImage(models.Model):
-#     ...
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='photos')
 
 
 class Like(models.Model):
-    ...
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+
+    def __str__(self):
+        return f'{self.author.username} liked {self.post}'
 
 
 class Comment(models.Model):
-    ...
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.username}-{self.text[:50]}'
